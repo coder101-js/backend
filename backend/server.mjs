af4f414c419058151348fa10b6fedd2b3a8d0874
='coder101_js <='op422010@gmail.com>
2025-07-01 15:06:22 +0500

    fix

--- backend/server.mjs
+++ backend/server.mjs
@@ -14,9 +14,9 @@ import middleMan from "./middleMan.mjs";
 app.use(
   cors({
     origin: [
-      "http://localhost:5500",
-      "http://127.0.0.1:5500",
-      "http://localhost:5501",
+      "http://localhost:3000", // dev
+      "https://api.buttnetworks.com", // API subdomain
+      "https://buttnetworks.com", // main frontend site
     ],
     credentials: true,
   })
