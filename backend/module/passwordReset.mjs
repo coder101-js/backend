9fb9d332bb6334ae2b0fb43f4c9be908ed94179c
='coder101_js <='op422010@gmail.com>
2025-07-01 22:22:18 +0500

    de

--- backend/module/passwordReset.mjs
+++ backend/module/passwordReset.mjs
@@ -11,41 +11,53 @@ import { sendPasswordResetConfirmation } from "./mail.mjs";
 import { hash } from "./hasing.mjs";
 
 export const sendResetPassword = async (req, res) => {
-  try {
-    
-    const { email } = req.body;
-    if (!email) {
-      return res.status(400).send({ err: "Email is required" });
-    }
-    console.log("Mongoose readyState:", mongoose.connection.readyState);
-    console.log("User model:", User);
-    console.log("✅ Mongoose connected:", mongoose.connection.readyState); // should be 1
-    console.log("📦 DB Name:", mongoose.connection.name); // should be userData
-    console.log("🔍 Looking for:", email);
+  import mongoose from "mongoose";
 
-    const user = await User.findOne({ email });
+  const uri = "mongodb://127.0.0.1:27017/userData";
+  await mongoose.connect(uri);
+  console.log("✅ connected");
 
-    if (!user) {
-      // Don't expose if user doesn't exist
-      return res
-        .status(200)
-        .send({ msg: "If that email exists, we sent a reset link " });
-    }
+  const schema = new mongoose.Schema({ email: String });
+  const User = mongoose.model("User", schema);
 
-    const resetToken = generatePasswordResetToken(user.email);
-    const passwordReset = new reset({
-      email,
-      resetToken,
-    });
-    await passwordReset.save();
+  const user = await User.findOne({});
+  console.log("🔍 Found:", user);
 
-    await emailType("forgot", email, resetToken);
+  // try {
 
-    return res.status(200).send({ msg: "Password reset link sent " });
-  } catch (err) {
-    console.error("Reset error:", err);
-    return res.status(500).send({ err: "Something went wrong " });
-  }
+  //   const { email } = req.body;
+  //   if (!email) {
+  //     return res.status(400).send({ err: "Email is required" });
+  //   }
+  //   console.log("Mongoose readyState:", mongoose.connection.readyState);
+  //   console.log("User model:", User);
+  //   console.log("✅ Mongoose connected:", mongoose.connection.readyState); // should be 1
+  //   console.log("📦 DB Name:", mongoose.connection.name); // should be userData
+  //   console.log("🔍 Looking for:", email);
+
+  //   const user = await User.findOne({ email });
+
+  //   if (!user) {
+  //     // Don't expose if user doesn't exist
+  //     return res
+  //       .status(200)
+  //       .send({ msg: "If that email exists, we sent a reset link " });
+  //   }
+
+  //   const resetToken = generatePasswordResetToken(user.email);
+  //   const passwordReset = new reset({
+  //     email,
+  //     resetToken,
+  //   });
+  //   await passwordReset.save();
+
+  //   await emailType("forgot", email, resetToken);
+
+  //   return res.status(200).send({ msg: "Password reset link sent " });
+  // } catch (err) {
+  //   console.error("Reset error:", err);
+  //   return res.status(500).send({ err: "Something went wrong " });
+  // }
 };
 
 export const validateResetRequest = async (req, res) => {
